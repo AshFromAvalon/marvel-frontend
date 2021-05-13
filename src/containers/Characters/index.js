45bfd4a9f7caa42292f505200f0792446f49065f
Thomas Le Bihan <thomas.66days@gmail.com>
2021-05-13 11:32:12 +0200

    pagination characters

--- src/containers/Characters/index.js
+++ src/containers/Characters/index.js
@@ -7,12 +7,13 @@ import axios from "axios";
 const Characters = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState({});
-
+  const [limit, setLimit] = useState(15);
+  const [skip, setSkip] = useState(0);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://react-marvel-backend.herokuapp.com/characters"
+          `https://react-marvel-backend.herokuapp.com/characters?limit=${limit}&skip=${skip}`
         );
         console.log(response.data);
         setData(response.data);
@@ -22,19 +23,37 @@ const Characters = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [limit, skip]);
 
+  const handleNextClick = () => {
+    setLimit(limit + 15);
+    setSkip(skip + 15);
+  };
+
+  const handlePreviousClick = () => {
+    setLimit(limit - 15);
+    setSkip(skip - 15);
+  };
   return !isLoading ? (
-    <div>
-      {data.results.map((character, index) => {
+    <div style={{ display: "flex", width: "50vw", flexWrap: "wrap" }}>
+      {data.map((character, index) => {
         return (
-          <img
-            key={index}
-            src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-            alt=""
-          />
+          <>
+            <img
+              style={{ height: 50 }}
+              key={index}
+              src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+              alt=""
+            />
+            <p>{character.name}</p>
+          </>
         );
       })}
+
+      <div>
+        {limit > 15 && <button onClick={handlePreviousClick}>précédent</button>}
+        {limit < 100 && <button onClick={handleNextClick}>suivant</button>}
+      </div>
     </div>
   ) : (
     <div>Loading...</div>
