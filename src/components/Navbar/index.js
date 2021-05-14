ac871dd8eea49d2feda39b25cd3afce6d58d9e52
Thomas Le Bihan <thomas.66days@gmail.com>
2021-05-14 13:20:13 +0200

    navigation link

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,45 @@
 import "./style.navbar.scss";
 
+// Dependencies
 import logo from "../../assets/images/Marvel_Logo.png";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+  console.log(location.pathname);
+
   return (
     <div className="navbar">
       <div className="navbar-container">
         <img src={logo} alt="" className="navbar-logo" />
+        <nav className="nav">
+          <Link
+            to="/"
+            className={
+              location.pathname === "/" ? "nav-link active" : "nav-link"
+            }
+          >
+            Characters
+          </Link>
+          <Link
+            to="/comics/"
+            className={
+              location.pathname === "/comics/" ? "nav-link active" : "nav-link"
+            }
+          >
+            Comics
+          </Link>
+          <Link
+            to="/favorites/"
+            className={
+              location.pathname === "/favorites/"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Favorites
+          </Link>
+        </nav>
       </div>
     </div>
   );
